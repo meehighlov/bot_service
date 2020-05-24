4e277bdff4419668ea98ef21cfb48a6742a623f9
John Mikhailov <mikhailovj@gmail.com>
2020-05-24 13:38:00 +0300

    call back api testing

--- service/api/v1/views.py
+++ service/api/v1/views.py
@@ -1,5 +1,14 @@
 from service.api.v1.blueprint import v1_blueprint
-from flask import jsonify
+from flask import jsonify, request
+
+from vk_api.utils import get_random_id
+
+from service.app.vk.auth import session_factory_with_auth
+
+
+session = session_factory_with_auth(
+    token=''
+)
 
 
 @v1_blueprint.route('/health', methods=['GET'])
@@ -7,6 +16,24 @@ def health():
     return jsonify({'status': 'ok'})
 
 
-@v1_blueprint.route('/info', methods=['GET'])
-def info():
-    pass
+@v1_blueprint.route('/vk_request_handler', methods=['POST'])
+def vk_handler():
+    api = session().get_api()
+
+    data = request.get_json()
+
+    if not data or 'type' not in data:
+        return 400
+
+    if data['type'] == 'confirmation':
+        return '42a82a6f'
+    elif data['type'] == 'message_new':
+        print(data)
+        from_id = data['object']['message']['from_id']
+        # отправляем сообщение
+        api.messages.send(
+            message='stub message',
+            random_id=get_random_id(),
+            peer_id=from_id
+        )
+        return 200
--- service/app/vk/auth.py
+++ service/app/vk/auth.py
@@ -6,6 +6,7 @@ from service.app.config import config
 def auth_vk(session):
     try:
         session.auth(token_only=True)
+        print('new auth')
     except vk_api.AuthError:
         # log it or something
         raise
@@ -18,3 +19,19 @@ def get_api():
     )
     auth_vk(session)
     return session.get_api()
+
+
+def session_factory_with_auth(vk_login=None, vk_password=None, token=None):
+    # vk_session = vk_api.VkApi(
+    #     vk_login,
+    #     vk_password
+    # ) if token is None else vk_api.VkApi(token=token)
+
+    # auth_vk(vk_session)
+
+    vk_session = vk_api.VkApi(token=token)
+
+    def session_():
+        return vk_session
+
+    return session_
