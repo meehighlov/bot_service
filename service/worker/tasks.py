e1cfabb8fddee4ed815fb9a20af269a670010768
John Mikhailov <mikhailovj@gmail.com>
2020-05-31 15:37:25 +0300

    remove using of fk lib

--- service/worker/tasks.py
+++
@@ -1,7 +0,0 @@
-from service.app.celery import app
-from service.app.vk.subscriptions import drop_all_friends_request
-
-
-@app.task
-def unlink_unsubscribed_users_task():
-    drop_all_friends_request()
