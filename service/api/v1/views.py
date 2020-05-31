b305216b9ec12cc41087ef25427e9b3b79640883
John Mikhailov <mikhailovj@gmail.com>
2020-05-31 16:11:39 +0300

    add chat view - works nice

--- service/api/v1/views.py
+++ service/api/v1/views.py
@@ -1,5 +1,10 @@
+import requests
+
 from service.api.v1.blueprint import v1_blueprint
 from flask import jsonify
+from flask import request
+
+from service.app.config import config
 
 
 @v1_blueprint.route('/health', methods=['GET'])
@@ -10,3 +15,26 @@ def health():
 @v1_blueprint.route('/info', methods=['GET'])
 def info():
     pass
+
+
+@v1_blueprint.route('/chat', methods=['POST'])
+def chat_view():
+    data = request.get_json()
+
+    if data['type'] == 'confirmation':
+        return config.CONFIRMATION_CODE
+
+    if data['type'] == 'message_new':
+        user_sender = data['object']['message']['from_id']
+
+        response_data = {
+            'message': 'stub message',
+            'peer_id': user_sender,
+            'access_token': config.BOT_TOKEN,
+            'v': '5.103',
+            'random_id': '0'
+        }
+
+        r = requests.get('https://api.vk.com/method/messages.send', params=response_data)
+
+    return 'ok'
--- service/app/config.py
+++ service/app/config.py
@@ -10,6 +10,8 @@ class Config:
 
     VK_LOGIN = os.getenv('VK_LOGIN')
     VK_PASSWORD = os.getenv('VK_PASSWORD')
+    BOT_TOKEN = os.getenv('BOT_TOKEN')
+    CONFIRMATION_CODE = os.getenv('CONFIRMATION_CODE')
 
     CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
     WORKER_SLEEP_TIME_SECONDS_DEFAULT = 60 * 60 * 24 * 3  # three days
